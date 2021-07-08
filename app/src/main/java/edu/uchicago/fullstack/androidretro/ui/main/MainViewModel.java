a02baab6f98a1c34453512faeb40a55039628c83
Nicholas Maheshwari <nicholasmaheshwari@gmail.com>
2021-07-08 14:37:20 -0700

    First commit

---
+++ app/src/main/java/edu/uchicago/fullstack/androidretro/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package edu.uchicago.fullstack.androidretro.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
